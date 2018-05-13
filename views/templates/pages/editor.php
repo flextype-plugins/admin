24c959eb053c60eb56b43191bb227bca6b5d231a
Awilum <awilum@msn.com>
2018-05-13 17:22:44 +0300

    Create editor form using Form and Html components

--- views/templates/pages/editor.php
+++ views/templates/pages/editor.php
@@ -1,21 +1,25 @@
 <?php
+
 namespace Flextype;
-use Flextype\Component\{I18n\I18n, Registry\Registry};
+
+use Flextype\Component\{I18n\I18n, Registry\Registry, Html\Html, Form\Form};
+
+Themes::template('admin/views/partials/head')->display();
+
+// Create editor form using Form and Html components
+echo (
+    Html::heading($page_frontmatter_data['title'], 2, ['class' => 'page-heading']).
+    Form::open().
+    Form::hidden('slug', $page_slug).
+    Html::heading($page_frontmatter_data['title'], 3).
+    Form::textarea('frontmatter', $page_frontmatter).
+    Html::br().
+    Html::heading('Content', 3).
+    Form::textarea('editor', $page_content).
+    Form::submit('save_page', I18n::find('admin_save', 'admin', Registry::get('site.locale')), ['class' => 'btn btn-black']).
+    Form::close()
+);
+
+Themes::template('admin/views/partials/footer')->display();
+
 ?>
-<?php Themes::template('admin/views/partials/head')->display(); ?>
-
-<h2 class="page-heading">
-    Edit page: <?php echo $page_frontmatter_data['title']; ?>
-</h2>
-
-<form method="post">
-    <input id="slug" name="slug" type="hidden" value="<?php echo $page_slug; ?>">
-    <h3>Frontmatter</h3>
-    <textarea id="frontmatter" name="frontmatter" rows="0" cols="0"><?php echo $page_frontmatter; ?></textarea>
-    <br>
-    <h3>Content</h3>
-    <textarea id="editor" name="editor" rows="8" cols="80"><?php echo $page_content; ?></textarea>
-    <button class="btn btn-lg btn-dark" name="save_page" type="submit"><?php echo I18n::find('admin_save', 'admin', Registry::get('site.locale')); ?></button>
-</form>
-
-<?php Themes::template('admin/views/partials/footer')->display(); ?>

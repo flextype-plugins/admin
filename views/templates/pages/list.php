0a9b1637a6364947c61e39c4105e47fb7a294744
Awilum <awilum@msn.com>
2018-05-13 18:22:11 +0300

    Add new translates for Pages page

--- views/templates/pages/list.php
+++ views/templates/pages/list.php
@@ -1,20 +1,21 @@
 <?php
 namespace Flextype;
-use Flextype\Component\{Http\Http, Registry\Registry};
+use Flextype\Component\{Http\Http, Registry\Registry, I18n\I18n};
 ?>
+
 
 <?php Themes::template('admin/views/partials/head')->display(); ?>
 
 <h2 class="page-heading">
-    Pages
-    <a class="btn pull-right btn-black" href="<?php echo Http::getBaseUrl(); ?>/admin/pages/add" role="button">Create New Page</a>
+    <?php echo I18n::find('admin_pages_heading', 'admin', Registry::get('site.locale')); ?>
+    <a class="btn pull-right btn-black" href="<?php echo Http::getBaseUrl(); ?>/admin/pages/add" role="button"><?php echo I18n::find('admin_pages_create_new', 'admin', Registry::get('site.locale')); ?></a>
 </h2>
 
 <table class="table">
   <thead>
     <tr>
-      <th scope="col">Name</th>
-      <th scope="col">Url</th>
+      <th scope="col"><?php echo I18n::find('admin_pages_name', 'admin', Registry::get('site.locale')); ?></th>
+      <th scope="col"><?php echo I18n::find('admin_pages_url', 'admin', Registry::get('site.locale')); ?></th>
       <th scope="col"></th>
     </tr>
   </thead>
@@ -23,9 +24,10 @@ use Flextype\Component\{Http\Http, Registry\Registry};
     <tr>
       <td scope="row"><a href="<?php echo Http::getBaseUrl(); ?>/admin/pages/edit?page=<?php if ($page['slug'] != '') echo $page['slug']; else echo Registry::get('site.pages.main'); ?>"><?php echo $page['title']; ?></a></td>
       <td scope="row"><a href="<?php echo Http::getBaseUrl(); ?>/<?php echo $page['slug']; ?>">/<?php echo $page['slug']; ?></a></td>
-      <td scope="row" class="text-right"><a href="<?php echo Http::getBaseUrl(); ?>/admin/pages/delete?page=<?php echo $page['slug']; ?>">Delete</a></td>
+      <td scope="row" class="text-right"><a href="<?php echo Http::getBaseUrl(); ?>/admin/pages/delete?page=<?php echo $page['slug']; ?>"><?php echo I18n::find('admin_pages_delete', 'admin', Registry::get('site.locale')); ?></a></td>
     </tr>
     <?php } ?>
   </tbody>
 </table>
+
 <?php Themes::template('admin/views/partials/footer')->display(); ?>

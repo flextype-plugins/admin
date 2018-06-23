677e291f31c90e2e7e9601286783a14c34ae66f1
Awilum <awilum@msn.com>
2018-06-23 03:20:06 +0300

    SHOW Flextype version in the footer

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -4,7 +4,7 @@ namespace Flextype;
     <?php Themes::view('admin/views/partials/tail')->display(); ?>
 
     <div class="powered pull-right">
-        Official Support Forum / Documentation / © 2018 Flextype – Version 0.3.0
+        Official Support Forum / Documentation / © 2018 Flextype – Version <?php echo Flextype::VERSION; ?>
     </div>
     </main>
   </body>

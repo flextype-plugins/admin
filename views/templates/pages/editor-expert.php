85b0723564e6f5c77445535c0bd4d30042bc1d3d
Awilum <awilum@msn.com>
2018-06-27 14:21:37 +0300

    Pages manger: working on the expert editor mode

---
+++ views/templates/pages/editor-expert.php
@@ -0,0 +1,32 @@
+<?php
+namespace Flextype;
+use Flextype\Component\{I18n\I18n, Registry\Registry, Html\Html, Form\Form, Http\Http};
+?>
+
+<?php Themes::view('admin/views/partials/head')->display(); ?>
+
+<form method="post">
+    <div class="row">
+      <div class="col-12">
+        <?php echo Form::hidden('slug', $page_slug); ?>
+        <div class="dark-panel">
+            <div class="dark-panel-header">
+                <h3 class="h3">
+                    <?php echo I18n::find('admin_pages_edit', 'admin', Registry::get('site.locale')); ?>
+                    <a href="<?php echo Http::getBaseUrl(); ?>/admin/pages/edit?page=<?php echo $page_slug; ?>" class="float-right">Switch back to editor mode</a>
+                </h3>
+            </div>
+            <div class="dark-panel-body">
+                <div class="form-group">
+                  <?php echo Form::textarea('editor-codemirror', $page_content); ?>
+                </div>
+            </div>
+            <div class="dark-panel-footer">
+                <button class="btn btn-black btn-editor btn-block" name="save_page_expert" type="submit"><?php echo I18n::find('admin_save', 'admin', Registry::get('site.locale')); ?></button>
+            </div>
+        </div>
+      </div>
+      </div>
+</form>
+
+<?php Themes::view('admin/views/partials/footer')->display(); ?>

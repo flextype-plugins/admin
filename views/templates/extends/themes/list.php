b4b9deced5f3046ef6f4fb90281823fab689cd42
Awilum <awilum@msn.com>
2018-10-26 02:59:32 +0300

    General code refactoring

---
+++ views/templates/extends/themes/list.php
@@ -0,0 +1,16 @@
+<?php
+namespace Flextype;
+use Flextype\Component\{Http\Http, Registry\Registry, I18n\I18n, Token\Token};
+?>
+
+<?php
+    Themes::view('admin/views/partials/head')->display();
+    Themes::view('admin/views/partials/content-start')->display();
+?>
+
+This area is not ready yet...
+
+<?php
+    Themes::view('admin/views/partials/content-end')->display();
+    Themes::view('admin/views/partials/footer')->display();
+?>

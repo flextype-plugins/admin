632991f1ffc1d6a87864603c13d80091f7b7fd16
Awilum <awilum@msn.com>
2018-11-10 03:03:12 +0300

    Form Generator - removed from current release

--- views/templates/system/settings/list.php
+++ views/templates/system/settings/list.php
@@ -171,26 +171,6 @@ use Flextype\Component\{Http\Http, Registry\Registry, I18n\I18n, Token\Token, Fo
     </div>
 </div>
 
-
-
-<div class="row">
-    <div class="col-md-6">
-        <?php Formgenerator::display(Registry::get('plugins.admin.forms.site_settings'), $site_settings); ?>
-    </div>
-<pre>
-<?php
-    print_r($site_settings);
-?>
-<?php
-    print_r($system_settings);
-?>
-</pre>
-
-    <div class="col-md-6">
-        <?php Formgenerator::display(Registry::get('plugins.admin.forms.system_settings'), $system_settings); ?>
-    </div>
-</div>
-
 <?php
     Themes::view('admin/views/partials/content-end')->display();
     Themes::view('admin/views/partials/footer')->display();

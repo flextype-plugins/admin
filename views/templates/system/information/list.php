691b941b6dba231df286523c97d81abc7347a6d7
Awilum <awilum@msn.com>
2018-11-20 22:46:02 +0300

    Fix Translates

--- views/templates/system/information/list.php
+++ views/templates/system/information/list.php
@@ -13,22 +13,22 @@ use Flextype\Component\{Http\Http, Registry\Registry, I18n\I18n, Token\Token};
 
 <div class="card">
     <div class="card-header">
-        System
+        <?php echo I18n::find('admin_system', Registry::get('system.locale')); ?>
     </div>
     <div class="card-body no-padding">
         <table class="table no-margin">
             <tbody>
                 <tr>
-                    <td>Flextype Version</td>
+                    <td><?php echo I18n::find('admin_flextype_version', Registry::get('system.locale')); ?></td>
                     <td><?php echo Flextype::VERSION; ?></td>
                 </tr>
                 <tr>
-                    <td>Debugging</td>
-                    <td><?php if (Registry::get('system.errors.display')) echo 'on'; else echo 'off'; ?></td>
+                    <td><?php echo I18n::find('admin_debugging', Registry::get('system.locale')); ?></td>
+                    <td><?php if (Registry::get('system.errors.display')) echo I18n::find('admin_on', Registry::get('system.locale')); else echo I18n::find('admin_off', Registry::get('system.locale')); ?></td>
                 </tr>
                 <tr>
-                    <td>Cache</td>
-                    <td><?php if (Registry::get('system.cache.enabled')) echo 'on'; else echo 'off'; ?></td>
+                    <td><?php echo I18n::find('admin_cache', Registry::get('system.locale')); ?></td>
+                    <td><?php if (Registry::get('system.cache.enabled')) echo I18n::find('admin_on', Registry::get('system.locale')); else echo I18n::find('admin_off', Registry::get('system.locale')); ?></td>
                 </tr>
             </tbody>
         </table>
@@ -37,43 +37,42 @@ use Flextype\Component\{Http\Http, Registry\Registry, I18n\I18n, Token\Token};
 
 <div class="card">
     <div class="card-header">
-        Server
+        <?php echo I18n::find('admin_server', Registry::get('system.locale')); ?>
     </div>
     <div class="card-body no-padding">
         <table class="table no-margin">
             <tbody>
                 <tr>
-                    <td>PHP version</td>
+                    <td><?php echo I18n::find('admin_php_version', Registry::get('system.locale')); ?></td>
                     <td><?php echo PHP_VERSION; ?></td>
                 </tr>
                 <tr>
-                    <td>PHP Built On</td>
+                    <td><?php echo I18n::find('admin_php_built_on', Registry::get('system.locale')); ?></td>
                     <td><?php echo php_uname(); ?></td>
                 </tr>
                 <tr>
-                    <td>Web Server</td>
+                    <td><?php echo I18n::find('admin_web_server', Registry::get('system.locale')); ?></td>
                     <td><?php echo (isset($_SERVER['SERVER_SOFTWARE'])) ? $_SERVER['SERVER_SOFTWARE'] : @getenv('SERVER_SOFTWARE'); ?></td>
                 </tr>
                 <tr>
-                    <td>WebServer to PHP Interface</td>
+                    <td><?php echo I18n::find('admin_web_server_php_interface', Registry::get('system.locale')); ?></td>
                     <td><?php echo php_sapi_name(); ?></td>
                 </tr>
                 <?php
                     if (function_exists('apache_get_modules')) {
                         if ( ! in_array('mod_rewrite',apache_get_modules())) {
-                            echo '<tr><td>'.'Apache Mod Rewrite'.'</td><td>'.'Not Installed'.'</td></tr>';
+                            echo '<tr><td>'.'Apache Mod Rewrite'.'</td><td>'.I18n::find('admin_not_installed', Registry::get('system.locale')).'</td></tr>';
                         } else {
-                            echo '<tr><td>'.'Apache Mod Rewrite'.'</td><td>'.'Installed'.'</td></tr>';
+                            echo '<tr><td>'.'Apache Mod Rewrite'.'</td><td>'.I18n::find('admin_installed', Registry::get('system.locale')).'</td></tr>';
                         }
                     } else {
-                        echo '<tr><td>'.'Apache Mod Rewrite'.'</td><td>'.'Installed'.'</td></tr>';
+                        echo '<tr><td>'.'Apache Mod Rewrite'.'</td><td>'.I18n::find('admin_installed', Registry::get('system.locale')).'</td></tr>';
                     }
                 ?>
             </tbody>
         </table>
     </div>
 </div>
-
 
 <?php
     Themes::view('admin/views/partials/content-end')->display();

8db1263ca04fb9672fb1903fd625cf43e615efa8
Awilum <awilum@msn.com>
2018-11-28 23:29:16 +0300

    General: Admin new Dark and Light theme! #30

--- views/templates/content/pages/add.php
+++ views/templates/content/pages/add.php
@@ -8,7 +8,7 @@ use function Flextype\Component\I18n\__;
     Themes::view('admin/views/partials/head')->display();
     Themes::view('admin/views/partials/navbar')
         ->assign('links', ['pages' => ['url' => Http::getBaseUrl() . '/admin/pages', 'title' => __('admin_pages_heading')],
-                                      ['url' => '#', 'title' => __('admin_pages_create_new')]])
+                           'pages_add' => ['url' => Http::getBaseUrl() . '/admin/pages/add', 'title' => __('admin_pages_create_new'), 'class' => 'active']])
         ->display();
     Themes::view('admin/views/partials/content-start')->display();
 ?>

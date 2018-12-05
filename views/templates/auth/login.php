58fecfa34e89e3ed44a0657832e1f43803b1ae45
Awilum <awilum@msn.com>
2018-12-05 22:51:36 +0300

    Code refactoring and formating fixes - Auth: login page

--- views/templates/auth/login.php
+++ views/templates/auth/login.php
@@ -1,44 +1,39 @@
 <?php
 namespace Flextype;
-use Flextype\Component\{Registry\Registry, Http\Http, Form\Form, Token\Token};
+
+use Flextype\Component\Registry\Registry;
+use Flextype\Component\Http\Http;
+use Flextype\Component\Form\Form;
+use Flextype\Component\Token\Token;
 use function Flextype\Component\I18n\__;
-?>
-<?php
-    Themes::view('admin/views/partials/head')
-            ->assign('main_panel_class', 'width-full')
-            ->display();
-    Themes::view('admin/views/partials/content-start')->display();
+
+Themes::view('admin/views/partials/head')
+        ->assign('main_panel_class', 'width-full')
+        ->display();
+Themes::view('admin/views/partials/content-start')->display();
 ?>
 
-<?php echo Form::open(); ?>
-<?php echo Form::hidden('token', Token::generate()); ?>
+<form action="" method="post">
+    <input type="hidden" id="token" name="token" value="<?php echo Token::generate(); ?>">
     <div class="row">
-        <div class="col-4 float-center">
-            <h3 class="h3"><?php echo __('admin_login'); ?></h3>
+        <div class="col-3 float-center">
+            <h3 class="h3 text-center"><?php echo __('admin_login'); ?></h3>
             <div class="form-group">
-                <?php
-                    echo (
-                        Form::label('username', __('admin_username'), ['for' => 'inputUsername']).
-                        Form::input('username', '', ['class' => 'form-control', 'id' => 'inputUsername', 'required'])
-                    );
-                ?>
+                <label><?php echo __('admin_username'); ?></label>
+                <input type="text" name="username" value="" class="form-control" required="required">
             </div>
             <div class="form-group">
-                <?php
-                    echo (
-                        Form::label('password', __('admin_password'), ['for' => 'inputPassword']).
-                        Form::password('password', '', ['class' => 'form-control', 'id' => 'inputPassword', 'required'])
-                    );
-                ?>
+                <label><?php echo __('admin_password'); ?></label>
+                <input type="password" name="password" value="" class="form-control" required="required">
             </div>
             <div class="form-group">
-                <?php echo Form::submit('login', __('admin_login'), ['class' => 'btn btn-black float-left']); ?>
+                <input type="submit" name="login" value="<?php echo __('admin_login'); ?>" class="btn btn-black btn-block">
             </div>
         </div>
     </div>
-<?php echo Form::open(); ?>
+</form>
 
 <?php
-    Themes::view('admin/views/partials/content-end')->display();
-    Themes::view('admin/views/partials/footer')->display();
+Themes::view('admin/views/partials/content-end')->display();
+Themes::view('admin/views/partials/footer')->display();
 ?>

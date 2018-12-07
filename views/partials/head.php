1948322e29a99dc4e9cd64dcd8882238ee1a9cbc
Awilum <awilum@msn.com>
2018-12-07 19:42:08 +0300

    General: Fix styles order prio

--- views/partials/head.php
+++ views/partials/head.php
@@ -25,12 +25,12 @@ use function Flextype\Component\I18n\__;
 
     <?php Assets::add('css', Http::getBaseUrl() . '/site/plugins/admin/assets/dist/css/bootstrap.min.css', 'admin', 1); ?>
     <?php Assets::add('css', Http::getBaseUrl() . '/site/plugins/admin/assets/js/trumbowyg/dist/ui/trumbowyg.min.css', 'admin', 2); ?>
-    <?php Assets::add('css', Http::getBaseUrl() . '/site/plugins/admin/assets/dist/css/admin.min.css', 'admin', 3); ?>
-    <?php Assets::add('css', Http::getBaseUrl() . '/site/plugins/admin/assets/dist/css/animate.min.css', 'admin', 4); ?>
-    <?php Assets::add('css', Http::getBaseUrl() . '/site/plugins/admin/assets/js/codemirror/lib/codemirror.css', 'admin', 5); ?>
-    <?php Assets::add('css', Http::getBaseUrl() . '/site/plugins/admin/assets/js/codemirror/theme/monokai.css', 'admin', 6); ?>
-    <?php Assets::add('css', Http::getBaseUrl() . '/site/plugins/admin/assets/js/messenger-hubspot/build/css/messenger.css', 'admin', 7); ?>
-    <?php Assets::add('css', Http::getBaseUrl() . '/site/plugins/admin/assets/js/messenger-hubspot/build/css/messenger-theme-flat.css', 'admin', 8); ?>
+    <?php Assets::add('css', Http::getBaseUrl() . '/site/plugins/admin/assets/dist/css/animate.min.css', 'admin', 3); ?>
+    <?php Assets::add('css', Http::getBaseUrl() . '/site/plugins/admin/assets/js/codemirror/lib/codemirror.css', 'admin', 4); ?>
+    <?php Assets::add('css', Http::getBaseUrl() . '/site/plugins/admin/assets/js/codemirror/theme/monokai.css', 'admin', 5); ?>
+    <?php Assets::add('css', Http::getBaseUrl() . '/site/plugins/admin/assets/js/messenger-hubspot/build/css/messenger.css', 'admin', 6); ?>
+    <?php Assets::add('css', Http::getBaseUrl() . '/site/plugins/admin/assets/js/messenger-hubspot/build/css/messenger-theme-flat.css', 'admin', 7); ?>
+    <?php Assets::add('css', Http::getBaseUrl() . '/site/plugins/admin/assets/dist/css/admin.min.css', 'admin', 8); ?>
 
     <?php foreach (Assets::get('css', 'admin') as $assets_by_priorities) { foreach ($assets_by_priorities as $assets) { ?>
         <link href="<?php echo $assets['asset']; ?>" rel="stylesheet">

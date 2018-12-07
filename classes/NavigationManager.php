67879055e4119333470c1b5acd3899060e0a9e46
Awilum <awilum@msn.com>
2018-12-07 22:41:46 +0300

    Navigation Manager: added ability to store area and item information

--- classes/NavigationManager.php
+++ classes/NavigationManager.php
@@ -8,6 +8,8 @@ class NavigationManager
 {
     public static function addItem(string $area, string $item, string $title, string $link, array $attributes = [])
     {
+        Registry::set("admin_navigation.{$area}.{$item}.area", $area);
+        Registry::set("admin_navigation.{$area}.{$item}.item", $item);
         Registry::set("admin_navigation.{$area}.{$item}.title", $title);
         Registry::set("admin_navigation.{$area}.{$item}.link", $link);
         Registry::set("admin_navigation.{$area}.{$item}.attributes", $attributes);

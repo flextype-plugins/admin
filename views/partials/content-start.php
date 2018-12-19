55b73d950c1ab2b94d00982795603cf43d98ffcf
Awilum <awilum@msn.com>
2018-12-20 02:21:04 +0300

    Pages Manager: fix layout

--- views/partials/content-start.php
+++ views/partials/content-start.php
@@ -1,8 +1,9 @@
 <?php
 namespace Flextype;
 use Flextype\Component\Http\Http;
+use Flextype\Component\Arr\Arr;
 ?>
-<div class="content <?php if(Http::get('page') !== null): ?> page-editor <?php endif ?>">
+<div class="content <?php if(Arr::last(explode("/", Http::getUriString())) == 'edit'): ?> page-editor <?php endif ?>">
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">

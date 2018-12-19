4ec2d3530bc88ad768438d44c1f87dc47fd5c6f0
Awilum <awilum@msn.com>
2018-12-19 21:07:49 +0300

    General: move navigation buttons to the top.

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -4,37 +4,29 @@ use Flextype\Component\{Http\Http, Html\Html, Registry\Registry, Token\Token, Ar
 use function Flextype\Component\I18n\__;
 ?>
 
-<!-- Top Navbar -->
-<?php if(isset($links)) { ?>
+<?php if (isset($links) || isset($buttons)): ?>
 <nav class="navbar navbar-expand-lg navbar-fixed">
     <div class="container-fluid">
+        <?php if (isset($links)): ?>
         <div class="navbar-wrapper">
-            <?php foreach ($links as $link) { ?>
-                <?php echo Html::anchor($link['title'], $link['link'], $link['attributes']); ?>
-            <?php } ?>
+            <?php foreach ($links as $link):  ?>
+                <?= Html::anchor($link['title'], $link['link'], $link['attributes']) ?>
+            <?php endforeach ?>
         </div>
+        <?php endif ?>
+        <?php if (isset($buttons)): ?>
+        <div class="navbar-buttons">
+            <?php foreach ($buttons as $button): ?>
+                <?= Html::anchor($button['title'], $button['link'], $button['attributes']) ?>
+            <?php endforeach ?>
+        </div>
+        <?php endif ?>
     </div>
 </nav>
-<?php } ?>
-<!-- End Navbar -->
+<?php endif ?>
 
-<?php if ((Http::get('page') !== null) && (Arr::last(Http::getUriSegments()) !== 'move') && (Arr::last(Http::getUriSegments()) !== 'rename')) { ?>
+<?php if ((Http::get('page') !== null) && (Arr::last(Http::getUriSegments()) !== 'move') && (Arr::last(Http::getUriSegments()) !== 'rename')): ?>
 <div class="page-editor-heading">
-     <a target="_blank" href="<?php echo $page['url']; ?>">/<?php if($page['slug'] == '') echo Registry::get('settings.pages.main'); else echo $page['slug']; ?></a>
+     <a target="_blank" href="<?= $page['url'] ?>">/<?php if ($page['slug'] == '') echo Registry::get('settings.pages.main'); else echo $page['slug']; ?></a>
 </div>
-<?php } ?>
-
-<!-- Bottom Navbar -->
-<?php if(isset($buttons)) { ?>
-<nav class="navbar navbar-expand-lg navbar-fixed fixed-bottom">
-  <div class="container-fluid">
-      <div class="navbar-buttons">
-          <?php foreach ($buttons as $button) { ?>
-              <?php echo Html::anchor($button['title'], $button['link'], $button['attributes']); ?>
-          <?php } ?>
-      </div>
-  </div>
-</nav>
-<?php } ?>
-</nav>
-<!-- End Navbar -->
+<?php endif ?>

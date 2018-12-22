61f04ddd432336f0f5537a5f614a29a6e7cac79a
Awilum <awilum@msn.com>
2018-12-23 01:52:45 +0300

    Updates according to the php template syntax guidelines

--- views/templates/content/entries/move.php
+++ views/templates/content/entries/move.php
@@ -26,29 +26,19 @@ use function Flextype\Component\I18n\__;
 
 <div class="row">
     <div class="col-md-6">
-
-        <?php echo Form::open(); ?>
-        <?php echo Form::hidden('token', Token::generate()); ?>
-        <?php echo Form::hidden('entry_path_current', $entry_path_current); ?>
-        <?php echo Form::hidden('entry_parent_current', $entry_parent); ?>
-        <?php echo Form::hidden('name_current', $name_current); ?>
-
+        <?= Form::open() ?>
+        <?= Form::hidden('token', Token::generate()) ?>
+        <?= Form::hidden('entry_path_current', $entry_path_current) ?>
+        <?= Form::hidden('entry_parent_current', $entry_parent) ?>
+        <?= Form::hidden('name_current', $name_current) ?>
         <div class="form-group">
-           <?php
-               echo (
-                   Form::label('parent_entry', __('admin_entries_parent_entry')).
-                   Form::select('parent_entry', $entries_list, $entry_parent, array('class' => 'form-control'))
-               );
-           ?>
-         </div>
-
-         <?php echo Form::submit('move_entry', __('admin_save'), ['class' => 'btn btn-black btn-fill btn-wd']); ?>
-     <?php echo Form::close(); ?>
-
- </div>
+           <?= Form::label('parent_entry', __('admin_entries_parent_entry')) ?>
+           <?= Form::select('parent_entry', $entries_list, $entry_parent, array('class' => 'form-control')) ?>
+        </div>
+        <?= Form::submit('move_entry', __('admin_save'), ['class' => 'btn btn-black btn-fill btn-wd']) ?>
+        <?= echo Form::close() ?>
+    </div>
 </div>
 
-<?php
-    Themes::view('admin/views/partials/content-end')->display();
-    Themes::view('admin/views/partials/footer')->display();
-?>
+<?php Themes::view('admin/views/partials/content-end')->display() ?>
+<?php Themes::view('admin/views/partials/footer')->display() ?>

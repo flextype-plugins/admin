4f72d94961a438ebb0456fa726d2fabca5cbe628
Awilum <awilum@msn.com>
2018-12-22 03:45:33 +0300

    Pages Manager: Infrastructure changes - Using one level tree view for pages list

--- views/partials/content-start.php
+++ views/partials/content-start.php
@@ -2,8 +2,9 @@
 namespace Flextype;
 use Flextype\Component\Http\Http;
 use Flextype\Component\Arr\Arr;
+use Flextype\Component\Registry\Registry;
 ?>
-<div class="content <?php if(Arr::last(explode("/", Http::getUriString())) == 'edit'): ?> page-editor <?php endif ?>">
+<div class="content <?php if(Registry::get('sidebar_menu_item') == 'pages'): ?> page-editor <?php endif ?>">
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">

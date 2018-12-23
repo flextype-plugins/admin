89c973994b726b7022741b8845b3771c9046d431
Awilum <awilum@msn.com>
2018-12-24 02:26:54 +0300

    General: code cleanup and improvements

--- views/partials/content-start.php
+++ views/partials/content-start.php
@@ -4,7 +4,7 @@ use Flextype\Component\Http\Http;
 use Flextype\Component\Arr\Arr;
 use Flextype\Component\Registry\Registry;
 ?>
-<div class="content <?php if(in_array(Registry::get('sidebar_menu_item'), ['entries', 'templates', 'snippets', 'fieldsets'])): ?> entry-editor <?php endif ?>">
+<div class="content <?php if (in_array(Registry::get('sidebar_menu_item'), ['entries', 'templates', 'snippets', 'fieldsets'])): ?> entry-editor <?php endif ?>">
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">

3a4df497bd165a28608ab55b5f8fdc6011d38610
Awilum <awilum@msn.com>
2018-12-23 22:48:41 +0300

    Snippets Manager: implementation - next round

--- admin.php
+++ admin.php
@@ -48,7 +48,8 @@ if (Admin::isAdminArea()) {
         // Add navigation links
         NavigationManager::addItem('content', 'entries', '<i class="far fa-newspaper"></i>' . __('admin_menu_content_entries', Registry::get('settings.locale')), Http::getBaseUrl() . '/admin/entries', ['class' => 'nav-link']);
         NavigationManager::addItem('extends', 'fieldsets', '<i class="fas fa-list"></i>' . __('admin_menu_extends_fieldsets', Registry::get('settings.locale')), Http::getBaseUrl() . '/admin/fieldsets', ['class' => 'nav-link']);
-        NavigationManager::addItem('extends', 'templates', '<i class="far fa-file-code"></i>' . __('admin_menu_extends_templates', Registry::get('settings.locale')), Http::getBaseUrl() . '/admin/templates', ['class' => 'nav-link']);
+        NavigationManager::addItem('extends', 'templates', '<i class="fas fa-layer-group"></i>' . __('admin_menu_extends_templates', Registry::get('settings.locale')), Http::getBaseUrl() . '/admin/templates', ['class' => 'nav-link']);
+        NavigationManager::addItem('extends', 'snippets', '<i class="far fa-file-code"></i>' . __('admin_menu_extends_snippets', Registry::get('settings.locale')), Http::getBaseUrl() . '/admin/snippets', ['class' => 'nav-link']);
         NavigationManager::addItem('extends', 'plugins', '<i class="fas fa-plug"></i>' . __('admin_menu_extends_plugins', Registry::get('settings.locale')), Http::getBaseUrl() . '/admin/plugins', ['class' => 'nav-link']);
         NavigationManager::addItem('settings', 'settings', '<i class="fas fa-cog"></i>' . __('admin_menu_system_settings', Registry::get('settings.locale')), Http::getBaseUrl() . '/admin/settings', ['class' => 'nav-link']);
         NavigationManager::addItem('settings', 'infomation', '<i class="fas fa-info"></i>' . __('admin_menu_system_information', Registry::get('settings.locale')), Http::getBaseUrl() . '/admin/information', ['class' => 'nav-link']);
@@ -144,6 +145,7 @@ class Admin
         Http::getUriSegment(1) == 'entries'      and EntriesManager::getEntriesManager();
         Http::getUriSegment(1) == 'fieldsets'    and FieldsetsManager::getFieldsetsManager();
         Http::getUriSegment(1) == 'templates'    and TemplatesManager::getTemplatesManager();
+        Http::getUriSegment(1) == 'snippets'     and SnippetsManager::getSnippetsManager();
         Http::getUriSegment(1) == 'plugins'      and PluginsManager::getPluginsManager();
         Http::getUriSegment(1) == 'information'  and InformationManager::getInformationManager();
         Http::getUriSegment(1) == 'settings'     and SettingsManager::getSettingsManager();

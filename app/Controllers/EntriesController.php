4d73c4b1e42939fd77e73d0672e617d98a59ebe4
Awilum <awilum@msn.com>
2020-05-19 23:52:13 +0300

    fix(core): delete specific hidden fields from Form Plugin on Entry Edit Proccess

--- app/Controllers/EntriesController.php
+++ app/Controllers/EntriesController.php
@@ -865,10 +865,15 @@ class EntriesController extends Container
             $data = $request->getParsedBody();
 
             // Delete system fields
-            Arr::delete($data, 'slug');
-            Arr::delete($data, 'csrf_value');
-            Arr::delete($data, 'csrf_name');
-            Arr::delete($data, 'action');
+            isset($data['slug'])                  and Arr::delete($data, 'slug');
+            isset($data['csrf_value'])            and Arr::delete($data, 'csrf_value');
+            isset($data['csrf_name'])             and Arr::delete($data, 'csrf_name');
+            isset($data['action'])                and Arr::delete($data, 'action');
+            isset($data['trumbowyg-icons-path'])  and Arr::delete($data, 'trumbowyg-icons-path');
+            isset($data['trumbowyg-locale'])      and Arr::delete($data, 'trumbowyg-locale');
+            isset($data['flatpickr-date-format']) and Arr::delete($data, 'flatpickr-date-format');
+            isset($data['flatpickr-locale'])      and Arr::delete($data, 'flatpickr-locale');
+
 
             $data['published_by'] = Session::get('uuid');
 

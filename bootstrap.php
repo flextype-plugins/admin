bdd681cdf5b53230b278ecb8db77b4f3ccf68b8d
Awilum <awilum@msn.com>
2021-01-16 23:06:47 +0300

    Admin 1.9.1

--- bootstrap.php
+++ bootstrap.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /**
  * Flextype Admin Plugin
  *
- * @link http://digital.flextype.org
+ * @link https://flextype.org
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.

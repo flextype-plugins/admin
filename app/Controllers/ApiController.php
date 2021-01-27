fa07718025ee8b26440e8061b4287e0331a5a7eb
Awilum <awilum@msn.com>
2021-01-27 19:28:01 +0300

    feat(core): simplify api manager

--- app/Controllers/ApiController.php
+++ app/Controllers/ApiController.php
@@ -30,7 +30,8 @@ class ApiController
         $api_stats = ['entries' => $this->getStats('entries'),
                       'registry' => $this->getStats('registry'),
                       'images' => $this->getStats('images'),
-                      'media' => $this->getStats('media'),
+                      'files' => $this->getStats('files'),
+                      'folders' => $this->getStats('folders'),
                       'access' => $this->getStats('access')];
 
         return flextype('twig')->render(
@@ -94,6 +95,245 @@ class ApiController
         }
 
         return $response->withRedirect(flextype('router')->pathFor('admin.api.index'));
+    }
+
+    /**
+     * Entries Index page
+     *
+     * @param Request  $request  PSR7 request
+     * @param Response $response PSR7 response
+     */
+    public function tokens(Request $request, Response $response) : Response
+    {
+        $query = $request->getQueryParams();
+
+        $tokens = [];
+        $tokens_list = Filesystem::listContents(PATH['project'] . '/tokens/' . $query['api']);
+
+        flextype('registry')->set('workspace', ['icon' => ['name' => 'diagram-3', 'set' => 'bootstrap']]);
+
+        if (count($tokens_list) > 0) {
+            foreach ($tokens_list as $token) {
+                if ($token['type'] == 'dir' && Filesystem::has(PATH['project'] . '/tokens/' . $query['api'] . '/' . $token['dirname'] . '/token.yaml')) {
+                    $tokens[] = $token;
+                }
+            }
+        }
+
+        return flextype('twig')->render(
+            $response,
+            'plugins/admin/templates/system/api/tokens.html',
+            [
+                'menu_item' => 'api',
+                'api' => $query['api'],
+                'icons' => ['entries' => ['name' => 'newspaper', 'set' => 'bootstrap'],
+                            'registry' => ['name' => 'archive', 'set' => 'bootstrap'],
+                            'images' => ['name' => 'images', 'set' => 'bootstrap'],
+                            'files' =>  ['name' => 'file-text', 'set' => 'bootstrap'],
+                            'folders' => ['name' => 'folder', 'set' => 'bootstrap'],
+                            'access' => ['name' => 'people', 'set' => 'bootstrap'],
+                            ],
+                'tokens' => $tokens,
+                'links' =>  [
+                    'api' => [
+                        'link' => flextype('router')->pathFor('admin.api.index'),
+                        'title' => __('admin_api'),
+                    ],
+                    'api_tokens' => [
+                        'link' => flextype('router')->pathFor('admin.api.tokens') . '?api=' . $query['api'],
+                        'title' => __('admin_' . $query['api'])
+                    ],
+                ]
+            ]
+        );
+    }
+
+    /**
+     * Add token page
+     *
+     * @param Request  $request  PSR7 request
+     * @param Response $response PSR7 response
+     */
+    public function add(Request $request, Response $response) : Response
+    {
+        flextype('registry')->set('workspace', ['icon' => ['name' => 'diagram-3', 'set' => 'bootstrap']]);
+
+        $query = $request->getQueryParams();
+
+        return flextype('twig')->render(
+            $response,
+            'plugins/admin/templates/system/api/add.html',
+            [
+                'menu_item' => 'api',
+                'api' => $query['api'],
+                'links' =>  [
+                    'api' => [
+                        'link' => flextype('router')->pathFor('admin.api.index'),
+                        'title' => __('admin_api'),
+                    ],
+                    'api_tokens' => [
+                        'link' => flextype('router')->pathFor('admin.api.tokens') . '?api=' . $query['api'],
+                        'title' => __('admin_' . $query['api'])
+                    ],
+                ]
+            ]
+        );
+    }
+
+    /**
+     * Add new token - process
+     *
+     * @param Request  $request  PSR7 request
+     * @param Response $response PSR7 response
+     */
+    public function addProcess(Request $request, Response $response) : Response
+    {
+        // Get POST data
+        $post_data = $request->getParsedBody();
+
+        // Generate API token
+        $api_token = bin2hex(random_bytes(16));
+
+        $api_token_dir_path  = PATH['project'] . '/tokens/' . $post_data['api'] . '/' . $api_token;
+        $api_token_file_path = $api_token_dir_path . '/token.yaml';
+
+        if (! Filesystem::has($api_token_file_path)) {
+
+            Filesystem::createDir($api_token_dir_path);
+
+            // Generate UUID
+            $uuid = Uuid::uuid4()->toString();
+
+            // Get time
+            $time = date(flextype('registry')->get('flextype.settings.date_format'), time());
+
+            // Create API Token account
+            if (Filesystem::write(
+                $api_token_file_path,
+                flextype('serializers')->yaml()->encode([
+                    'title' => $post_data['title'],
+                    'icon' => ['name' => $post_data['icon_name'],
+                               'set' => $post_data['icon_set']],
+                    'limit_calls' => (int) $post_data['limit_calls'],
+                    'calls' => (int) 0,
+                    'state' => $post_data['state'],
+                    'uuid' => $uuid,
+                    'created_by' => flextype('session')->get('uuid'),
+                    'created_at' => $time,
+                    'updated_by' => flextype('session')->get('uuid'),
+                    'updated_at' => $time,
+                ])
+            )) {
+                flextype('flash')->addMessage('success', __('admin_message_' . $post_data['api'] . '_api_token_created'));
+            } else {
+                flextype('flash')->addMessage('error', __('admin_message_' . $post_data['api'] . '_api_token_was_not_created'));
+            }
+        } else {
+            flextype('flash')->addMessage('error', __('admin_message_' . $post_data['api'] . '_api_token_was_not_created'));
+        }
+
+        return $response->withRedirect(flextype('router')->pathFor('admin.api.tokens') . '?api=' . $post_data['api']);
+
+    }
+
+    /**
+     * Edit token page
+     *
+     * @param Request  $request  PSR7 request
+     * @param Response $response PSR7 response
+     */
+    public function edit(Request $request, Response $response) : Response
+    {
+        $query = $request->getQueryParams();
+
+        $token      = $request->getQueryParams()['token'];
+        $token_data = flextype('serializers')->yaml()->decode(Filesystem::read(PATH['project'] . '/tokens/' . $query['api'] . '/' . $token . '/token.yaml'));
+
+        flextype('registry')->set('workspace', ['icon' => ['name' => 'diagram-3', 'set' => 'bootstrap']]);
+
+        return flextype('twig')->render(
+            $response,
+            'plugins/admin/templates/system/api/edit.html',
+            [
+                'menu_item' => 'api',
+                'token' => $token,
+                'api' => $query['api'],
+                'token_data' => $token_data,
+                'links' =>  [
+                    'api' => [
+                        'link' => flextype('router')->pathFor('admin.api.index'),
+                        'title' => __('admin_api')
+                    ],
+                    'api_tokens' => [
+                        'link' => flextype('router')->pathFor('admin.api.tokens') . '?api=' . $query['api'],
+                        'title' => __('admin_' . $query['api'])
+                    ],
+                ]
+            ]
+        );
+    }
+
+    /**
+     * Edit token - process
+     *
+     * @param Request  $request  PSR7 request
+     * @param Response $response PSR7 response
+     */
+    public function editProcess(Request $request, Response $response) : Response
+    {
+        // Get POST data
+        $post_data = $request->getParsedBody();
+
+        $api_token_dir_path  = PATH['project'] . '/tokens/'. $post_data['api'] .'/' . $post_data['token'];
+        $api_token_file_path = $api_token_dir_path . '/' . 'token.yaml';
+
+        // Update API Token File
+        if (Filesystem::has($api_token_file_path)) {
+            if (Filesystem::write(
+                $api_token_file_path,
+                flextype('serializers')->yaml()->encode([
+                    'title' => $post_data['title'],
+                    'icon' => ['name' => $post_data['icon_name'],
+                               'set' => $post_data['icon_set']],
+                    'limit_calls' => (int) $post_data['limit_calls'],
+                    'calls' => (int) $post_data['calls'],
+                    'state' => $post_data['state'],
+                    'uuid' => $post_data['uuid'],
+                    'created_by' => $post_data['created_by'],
+                    'created_at' => $post_data['created_at'],
+                    'updated_by' => flextype('session')->get('uuid'),
+                    'updated_at' => date(flextype('registry')->get('flextype.settings.date_format'), time()),
+                ])
+            )) {
+                flextype('flash')->addMessage('success', __('admin_message_'. $post_data['api'] .'_api_token_updated'));
+            }
+        } else {
+            flextype('flash')->addMessage('error', __('admin_message_'. $post_data['api'] .'_api_token_was_not_updated'));
+        }
+
+        return $response->withRedirect(flextype('router')->pathFor('admin.api.tokens') . '?api=' . $post_data['api']);
+    }
+
+    /**
+     * Delete token - process
+     *
+     * @param Request  $request  PSR7 request
+     * @param Response $response PSR7 response
+     */
+    public function deleteProcess(Request $request, Response $response) : Response
+    {
+        // Get POST data
+        $post_data = $request->getParsedBody();
+
+        $api_token_dir_path = PATH['project'] . '/tokens/' . $post_data['api'] . '/' . $post_data['token'];
+
+        if (Filesystem::deleteDir($api_token_dir_path)) {
+            flextype('flash')->addMessage('success', __('admin_message_'. $post_data['api'] .'_api_token_deleted'));
+        } else {
+            flextype('flash')->addMessage('error', __('admin_message_'. $post_data['api'] .'_api_token_was_not_deleted'));
+        }
+
+        return $response->withRedirect(flextype('router')->pathFor('admin.api.tokens') . '?api=' . $post_data['api']);
     }
 
     private function getStats(string $api) {
--- routes/web.php
+++ routes/web.php
@@ -63,47 +63,12 @@ flextype()->group('/' . $admin_route, function () : void {
     flextype()->get('/api', ApiController::class . ':index')->setName('admin.api.index');
     flextype()->post('/api/delete-api-tokens', ApiController::class . ':deleteApiTokensProcess')->setName('admin.api.deleteApiTokensProcess');
 
-    flextype()->get('/api/entries', ApiEntriesController::class . ':index')->setName('admin.api_entries.index');
-    flextype()->get('/api/entries/add', ApiEntriesController::class . ':add')->setName('admin.api_entries.add');
-    flextype()->post('/api/entries/add', ApiEntriesController::class . ':addProcess')->setName('admin.api_entries.addProcess');
-    flextype()->get('/api/entries/edit', ApiEntriesController::class . ':edit')->setName('admin.api_entries.edit');
-    flextype()->post('/api/entries/edit', ApiEntriesController::class . ':editProcess')->setName('admin.api_entries.editProcess');
-    flextype()->post('/api/entries/delete', ApiEntriesController::class . ':deleteProcess')->setName('admin.api_entries.deleteProcess');
-
-    flextype()->get('/api/files', ApiFilesController::class . ':index')->setName('admin.api_files.index');
-    flextype()->get('/api/files/add', ApiFilesController::class . ':add')->setName('admin.api_files.add');
-    flextype()->post('/api/files/add', ApiFilesController::class . ':addProcess')->setName('admin.api_files.addProcess');
-    flextype()->get('/api/files/edit', ApiFilesController::class . ':edit')->setName('admin.api_files.edit');
-    flextype()->post('/api/files/edit', ApiFilesController::class . ':editProcess')->setName('admin.api_files.editProcess');
-    flextype()->post('/api/files/delete', ApiFilesController::class . ':deleteProcess')->setName('admin.api_files.deleteProcess');
-
-    flextype()->get('/api/folders', ApiFoldersController::class . ':index')->setName('admin.api_folders.index');
-    flextype()->get('/api/folders/add', ApiFoldersController::class . ':add')->setName('admin.api_folders.add');
-    flextype()->post('/api/folders/add', ApiFoldersController::class . ':addProcess')->setName('admin.api_folders.addProcess');
-    flextype()->get('/api/folders/edit', ApiFoldersController::class . ':edit')->setName('admin.api_folders.edit');
-    flextype()->post('/api/folders/edit', ApiFoldersController::class . ':editProcess')->setName('admin.api_folders.editProcess');
-    flextype()->post('/api/folders/delete', ApiFoldersController::class . ':deleteProcess')->setName('admin.api_folders.deleteProcess');
-
-    flextype()->get('/api/registry', ApiRegistryController::class . ':index')->setName('admin.api_registry.index');
-    flextype()->get('/api/registry/add', ApiRegistryController::class . ':add')->setName('admin.api_registry.add');
-    flextype()->post('/api/registry/add', ApiRegistryController::class . ':addProcess')->setName('admin.api_registry.addProcess');
-    flextype()->get('/api/registry/edit', ApiRegistryController::class . ':edit')->setName('admin.api_registry.edit');
-    flextype()->post('/api/registry/edit', ApiRegistryController::class . ':editProcess')->setName('admin.api_registry.editProcess');
-    flextype()->post('/api/registry/delete', ApiRegistryController::class . ':deleteProcess')->setName('admin.api_registry.deleteProcess');
-
-    flextype()->get('/api/images', ApiImagesController::class . ':index')->setName('admin.api_images.index');
-    flextype()->get('/api/images/add', ApiImagesController::class . ':add')->setName('admin.api_images.add');
-    flextype()->post('/api/images/add', ApiImagesController::class . ':addProcess')->setName('admin.api_images.addProcess');
-    flextype()->get('/api/images/edit', ApiImagesController::class . ':edit')->setName('admin.api_images.edit');
-    flextype()->post('/api/images/edit', ApiImagesController::class . ':editProcess')->setName('admin.api_images.editProcess');
-    flextype()->post('/api/images/delete', ApiImagesController::class . ':deleteProcess')->setName('admin.api_images.deleteProcess');
-
-    flextype()->get('/api/access', ApiAccessController::class . ':index')->setName('admin.api_access.index');
-    flextype()->get('/api/access/add', ApiAccessController::class . ':add')->setName('admin.api_access.add');
-    flextype()->post('/api/access/add', ApiAccessController::class . ':addProcess')->setName('admin.api_access.addProcess');
-    flextype()->get('/api/access/edit', ApiAccessController::class . ':edit')->setName('admin.api_access.edit');
-    flextype()->post('/api/access/edit', ApiAccessController::class . ':editProcess')->setName('admin.api_access.editProcess');
-    flextype()->post('/api/access/delete', ApiAccessController::class . ':deleteProcess')->setName('admin.api_access.deleteProcess');
+    flextype()->get('/api/tokens', ApiController::class . ':tokens')->setName('admin.api.tokens');
+    flextype()->get('/api/tokens/add', ApiController::class . ':add')->setName('admin.api.add');
+    flextype()->post('/api/tokens/add', ApiController::class . ':addProcess')->setName('admin.api.addProcess');
+    flextype()->get('/api/tokens/edit', ApiController::class . ':edit')->setName('admin.api.edit');
+    flextype()->post('/api/tokens/edit', ApiController::class . ':editProcess')->setName('admin.api.editProcess');
+    flextype()->post('/api/tokens/delete', ApiController::class . ':deleteProcess')->setName('admin.api.deleteProcess');
 
 })->add(new AclIsUserLoggedInMiddleware(['redirect' => 'admin.accounts.login']))
   ->add(new AclIsUserLoggedInRolesInMiddleware(['redirect' => (flextype('acl')->isUserLoggedIn() ? 'admin.accounts.no-access' : 'admin.accounts.login'),

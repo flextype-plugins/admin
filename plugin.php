d2e4ae96da63e29ce2a5c3c38c15deb6521b730b
Awilum <sergey.romanenko@flextype.org>
2021-07-12 21:08:07 +0300

    feat(plugin): general updates for admin panel navigation handling

--- plugin.php
+++ plugin.php
@@ -44,11 +44,7 @@ include_once 'routes/web.php';
 I18n::$locale = flextype('registry')->get('flextype.settings.locale');
 
 // Add Admin Navigation
-flextype('registry')->set('plugins.admin.settings.navigation.primary.content.entries', ['title' => __('admin_entries'), 'icon' => ['name' => 'newspaper', 'set' => 'bootstrap'], 'link' => flextype('router')->pathFor('admin.entries.index')]);
-flextype('registry')->set('plugins.admin.settings.navigation.primary.content.media', ['title' => __('admin_media'), 'icon' => ['name' => 'images', 'set' => 'bootstrap'], 'link' => flextype('router')->pathFor('admin.media.index')]);
-flextype('registry')->set('plugins.admin.settings.navigation.primary.extends.plugins', ['title' => __('admin_plugins'), 'icon' => ['name' => 'box', 'set' => 'bootstrap'], 'link' => flextype('router')->pathFor('admin.plugins.index')]);
-flextype('registry')->set('plugins.admin.settings.navigation.primary.system.tools', ['title' => __('admin_tools'), 'icon' => ['name' => 'briefcase', 'set' => 'bootstrap'], 'link' => flextype('router')->pathFor('admin.tools.index')]);
-flextype('registry')->set('plugins.admin.settings.navigation.primary.system.settings', ['title' => __('admin_settings'), 'icon' => ['name' => 'gear', 'set' => 'bootstrap'], 'link' => flextype('router')->pathFor('admin.settings.index')]);
-flextype('registry')->set('plugins.admin.settings.navigation.primary.system.api', ['title' => __('admin_api'), 'icon' => ['name' => 'diagram-3', 'set' => 'bootstrap'], 'link' => flextype('router')->pathFor('admin.api.index')]);
 flextype('registry')->set('assets.admin.css.admin', ['project/plugins/admin/assets/dist/css/admin.min.css']);
 flextype('registry')->set('assets.admin.js.admin', ['project/plugins/admin/assets/dist/js/admin.min.js']);
+
+
